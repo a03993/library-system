a9f452e5f47c9b64bb337eff2231ee6dacdfc29b
Tina <a03993@yahoo.com.tw>
2024-10-19 01:19:25 +0800

    Backend: Setup Express server and MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/library")
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
